0e8be42ad9ccb73ea0ebc5e00891ed34a8a133a7
Abhinav <abhi.gupta8802@gmail.com>
2023-08-24 07:57:49 +0530

    INIT

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import { MapScreen } from './screens/MapScreen';
+import { LocationPermissionsService } from './storage/LocationPermissionsService';
+import { ThemeProvider } from './theme/ThemeProvider';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { UserLocaitionStateContextProvider } from './context/UserLocationStateContext';
+
+
+export const App = () => {
+  return (
+    <SafeAreaProvider>
+      <ThemeProvider>
+        <UserLocaitionStateContextProvider>
+          <MapScreen />
+          <LocationPermissionsService />
+        </UserLocaitionStateContextProvider>
+      </ThemeProvider>
+    </SafeAreaProvider>
+  );
+};
+
